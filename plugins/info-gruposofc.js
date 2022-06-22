334647462bad529aed30dc9889acb56182db67ec
Ivan56x <107534753+Ivan56x@users.noreply.github.com>
2022-06-21 21:22:32 -0500

    Add files via upload

---
+++ plugins/info-gruposofc.js
@@ -0,0 +1,13 @@
+let media = './Menu2.jpg'
+let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
+*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de Luis King - Bot*
+
+*➤ Grupos oficiales del Bot:*
+*1.-* https://chat.whatsapp.com/H18N0f8seDsBdOyAuweUIT
+
+*2.-* https://chat.whatsapp.com/CViICouF14MCcpiRFBAm5l
+
+*3.-* https://chat.whatsapp.com/GkeP7U13SLMA2moZmRqtYd
+`.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
+handler.command = /^linkgc|grupos$/i
+export default handler
